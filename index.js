0d4bfb83cb2144c21d2ba93d87022b69b3a45ea3
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-06-15 14:09:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const number = 5;
 for (let index = 1; index <= 3; index++) {
     const element = index;
-    let guess = prompt("Guess a number between 1-10")
+    let guess = prompt("Guess a number between 1-10");
 
     if (guess == number) {
-        alert(`Sucess the number was ${number} ! and you did it in ${element} attempts.`);
-    } else { alert(`Wrong!!! Try Again!`) }
-    if (guess !== number && element === 3) { alert(`Sorry you failed to guess the number in three attempts. The number was ${number}`) };
+        alert(
+            `Sucess the number was ${number} ! and you did it in ${element} attempts.`
+        );
+        break
+    } else {
+        alert(`Wrong!!! Try Again!`);
+    }
+    if (guess !== number && element === 3) {
+        alert(
+            `Sorry you failed to guess the number in three attempts. The number was ${number}`
+        );
+    }
 };
